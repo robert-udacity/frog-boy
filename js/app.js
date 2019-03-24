6ef082f9491935bec682dd544f966bf03fcf44c4
robert <udacity@hellarobots.com>
2019-03-24 15:50:35 -0500

    Add enemy speed and movement

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -9,6 +9,7 @@ var Enemy = function(x, y) {
 
     this.x = 0;
     this.y = 0;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -17,6 +18,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    this.x = (this.x + this.speed * dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -40,11 +43,14 @@ Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+Player.prototype.handleInput = function(keyCode) {
+}
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-const bug1 = new Enemy(0, 0);
+const bug1 = new Enemy(0, 0, 40);
 const allEnemies = [bug1];
 const player = new Player();
 
