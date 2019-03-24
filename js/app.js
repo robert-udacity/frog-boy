54433f69c5a8b5461cd4aeb36117b0177a9aeb2c
robert <udacity@hellarobots.com>
2019-03-24 17:43:53 -0500

    Refactor to use ES6 classes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ const PLAYER_START_POSITION_X = 200;
 const PLAYER_START_POSITION_Y = 305;
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+class Enemy {
+  constructor (x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -15,89 +16,92 @@ var Enemy = function(x, y, speed) {
     this.height = 70;
     this.width = 80;
     this.speed = speed;
-};
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 
     this.x = (this.x + this.speed * dt) % 500;
     detectCollision(this, player);
-};
+  };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  // Draw the enemy on the screen, required method for game
+  render () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  };
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-function Player(x, y) {
-  this.sprite = 'images/char-boy.png';
-  this.x = x;
-  this.y = y;
-  this.height = 70;
-  this.width = 80;
-}
-
-Player.prototype.update = function(dt) {
-}
-
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-Player.prototype.handleInput = function(keyCode) {
-  // Moving up/down === ~82
-  // Moving left/right === 100
-  switch (keyCode) {
-    case 'up':
-      if ((this.y - 82) < -28) {
-        // reached the water, you win!
-      } else {
-        this.y = this.y - 82;
-      }
-
-      break;
-    case 'down':
-      // y-value of 400 is the bottom row
-      if (this.y + 82 > 400) {
-        // noop, stay at the bottom
-      } else {
-        this.y = this.y + 82;
-      }
-
-      break;
-    case 'left':
-      // x-value of 0 is the left edge
-      if (this.x - 100 < 0) {
-        // noop, stay at the left edge
-      } else {
-        this.x = this.x - 100;
-      }
-
-      break;
-    case 'right':
-      // x-value of 500 is the right edge
-      if (this.x + 100 >= 500) {
-        // noop, stay at the right edge
-      } else {
-        this.x = this.x + 100;
-      }
-
-      break;
-    default:
+class Player {
+  constructor (x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+    this.height = 70;
+    this.width = 80;
   }
-}
 
-Player.prototype.reset = function() {
-  this.x = PLAYER_START_POSITION_X;
-  this.y = PLAYER_START_POSITION_Y;
+  update (dt) {
+  }
+
+  render () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput (keyCode) {
+    // Moving up/down === ~82
+    // Moving left/right === 100
+    switch (keyCode) {
+      case 'up':
+        if ((this.y - 82) < -28) {
+          // reached the water, you win!
+        } else {
+          this.y = this.y - 82;
+        }
+
+        break;
+      case 'down':
+        // y-value of 400 is the bottom row
+        if (this.y + 82 > 400) {
+          // noop, stay at the bottom
+        } else {
+          this.y = this.y + 82;
+        }
+
+        break;
+      case 'left':
+        // x-value of 0 is the left edge
+        if (this.x - 100 < 0) {
+          // noop, stay at the left edge
+        } else {
+          this.x = this.x - 100;
+        }
+
+        break;
+      case 'right':
+        // x-value of 500 is the right edge
+        if (this.x + 100 >= 500) {
+          // noop, stay at the right edge
+        } else {
+          this.x = this.x + 100;
+        }
+
+        break;
+      default:
+    }
+  }
+
+  reset () {
+    this.x = PLAYER_START_POSITION_X;
+    this.y = PLAYER_START_POSITION_Y;
+  }
 }
 
 function detectCollision(object1, object2) {
